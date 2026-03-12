6ee17f89a1d9dd1a54bd5bf81d4a488a04ce14b8
sourav3223-crypto <sourav3223@icloud.com>
2026-03-12 23:47:41 +0530

    Create prisma.js

---
+++ src/utils/prisma.js
@@ -0,0 +1,4 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = global.prisma || new PrismaClient({ log: ['error'] });
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+module.exports = prisma;
